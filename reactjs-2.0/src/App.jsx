1cec4614ed7b94b855e6eb63641929ad6af046da
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-07-17 03:49:20 +0700

    menyelesaikan sesi 2

---
+++ reactjs-2.0/src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+import Header from "./components/Header";
+
+function App() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <>
+      <Header />
+      <button onClick={handleClick}>Likes ({likes})</button>
+    </>
+  );
+}
+
+export default App;
---
+++ reactjs-2.0/src/components/Header.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/prop-types */
+
+export default function Header({ author }) {
+  return <h1>Belajar Reactjs bareng {author ? author : "WPU "} 🚀</h1>;
+}
